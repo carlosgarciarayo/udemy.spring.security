a03fb844ccd57cf99184f6fbf6d5063bb380bba9
carlos <carlosgarciarayo201@gmail.com>
2025-12-02 12:20:44 +0100

    se modifica el application

--- src/main/java/udemy/spring/security/web/controllers/AboutUsController.java
+++ src/main/java/udemy/spring/security/web/controllers/AboutUsController.java
@@ -12,8 +12,6 @@ import java.util.Map;
 @RequestMapping(path = "/aboutUs")
 public class AboutUsController {
 
-
-
     @GetMapping
     public Map<String,String> aboutUs(){
         return Collections.singletonMap("msj","aboutUs");
