63f2f5b962453a5db9357f98fba1234acccd1755
carlos <carlosgarciarayo201@gmail.com>
2025-12-04 13:27:56 +0100

    se suben nuevos archivos

--- src/main/java/udemy/spring/security/web/controllers/AboutUsController.java
+++ src/main/java/udemy/spring/security/web/controllers/AboutUsController.java
@@ -1,6 +1,6 @@
 package udemy.spring.security.web.controllers;
 
-
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Collections;
 import java.util.Map;
 
+@Slf4j
 @RestController
 @RequestMapping(path = "/aboutUs")
 public class AboutUsController {
@@ -15,6 +16,7 @@ public class AboutUsController {
     @GetMapping
     public Map<String,String> aboutUs(){
 
+        log.debug("About Us endpoint called");
         return Collections.singletonMap("msj","aboutUs");
 
     }
